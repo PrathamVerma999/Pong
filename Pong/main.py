04ffe0ebe752cb59169d6d7c0f042ec81ce47bc0
Pratham Verma <110521546+PrathamVerma999@users.noreply.github.com>
2023-11-06 04:09:40 +0530

    pong prototype

--- Pong/main.py
+++ Pong/main.py
@@ -1,34 +1,32 @@
-from turtle import Turtle, Screen
-
+from turtle import Screen, Turtle
 
 screen = Screen()
-screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.setup(width=800, height=600)
 screen.title("PONG")
-
-position_x = -375
-position_y = 0
-segments = []
-
-for i in range(3):
-    paddle = Turtle()
-    paddle.shape("square")
-    paddle.color("white")
-    paddle.penup()
-    paddle.setpos(position_x, position_y)
-    position_y -=20
-
-
 screen.tracer(0)
 
+paddle = Turtle("square")
+paddle.color("white")
+paddle.penup()
+paddle.turtlesize(stretch_len=1, stretch_wid=5)
+paddle.setpos(x=350, y=0)
 
+def up():
+    new_ycor = paddle.ycor() + 20
+    paddle.setpos(350, new_ycor)
 
-
-
-
+def down():
+    new_ycor = paddle.ycor() - 20
+    paddle.setpos(350, new_ycor)
+    
+screen.listen()
+screen.onkey(up, "Up")
+screen.onkey(down, "Down")
 
 game_is_on = True
-
+while game_is_on:
+    screen.update()
 
 
 
